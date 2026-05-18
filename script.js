15552e106c0585f5f2c2a7082cc3beee2593c6fc
dpbueno106-cyber <dpbueno106@gmail.com>
2026-05-18 15:32:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function sayHello()  {
-  alert("You clicked the button!");
+function Login()  {
+  let username = document.getElementById("username").value ;
+  let password = document.getElementById("password").value;
+  let message = document.getElementById("message");
+  
 }
 //function settings() {
   
