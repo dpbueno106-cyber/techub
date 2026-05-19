7dfade615f34ca7c9613a5a93d4a2ae2f086ff21
dpbueno106-cyber <dpbueno106@gmail.com>
2026-05-19 08:54:09 -0400

    Update userDashboard.js

--- userDashboard.js
+++ userDashboard.js
@@ -1,2 +1,35 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { 
+      getAuth, 
+      signInWithEmailAndPassword,
+      createUserWithEmailAndPassword
+    } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyD9i5yfE80MAsiri8SwiRCFParRb9jPyzY",
+      authDomain: "techub-login-system.firebaseapp.com",
+      projectId: "techub-login-system",
+      storageBucket: "techub-login-system.firebasestorage.app",
+      messagingSenderId: "48424106638",
+      appId: "1:48424106638:web:9246d83f302b21ab0327df",
+      measurementId: "G-PQ5RJ1V0BB"
+    };
+
+    //Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+const welcome = document.getElementById("welcome");
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    welcome.textContent = "Welcome, " + user.email;
+  }
+  else
+  { //this might cause issues in future: beware daniel
+    window.location.href = "index.html";
+  }
+});
+
+
 
