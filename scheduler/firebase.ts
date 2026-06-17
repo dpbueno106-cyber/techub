64752929ce53624e7964025228405f6909f05239
dpbueno106-cyber <dpbueno106@gmail.com>
2026-06-17 11:13:52 -0400

    fix firebase admin imports

--- scheduler/firebase.ts
+++ scheduler/firebase.ts
@@ -1,44 +1,12 @@
-const admin = require("firebase-admin");
 
-let _savedSchedule: any[] | null = null;
 
-function makeStubDb() {
-  return {
-    collection() {
-      return {
-        doc() {
-          return {
-            async get() {
-              return {
-                exists: _savedSchedule !== null,
-                data: () => ({ schedule: _savedSchedule })
-              };
-            },
-            async set(payload: any) {
-              _savedSchedule = payload.schedule ?? payload;
-              return Promise.resolve();
-            }
-          };
-        }
-      };
-    }
-  };
-}
 
-let db: any;
+import * as admin from "firebase-admin";
 
-try {
-  const serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-
-  db = admin.firestore();
-
-} catch (err) {
-  console.log("Using stub DB");
-  db = makeStubDb();
-}
+const db = admin.firestore();
 
 export { db };
